df415e2389e96433caeb04fb60b06eb5e46cc489
JoshKarpel <josh.karpel@gmail.com>
2019-11-30 23:29:40 -0600

    day 01 in ruby

---
+++ ruby/day_01.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'pathname'
+
+def read_data
+  (Pathname(__dir__).parent / 'data' / 'day_01.txt').readlines.map(&:to_i)
+end
+
+def calculate_module_fuel(mass)
+  (mass / 3).truncate - 2
+end
+
+def part_one(data)
+  (data.map { |m| calculate_module_fuel(m) }).sum
+end
+
+def calculate_total_module_fuel(mass)
+  fuels = [calculate_module_fuel(mass)]
+  loop do
+    extra_fuel = calculate_module_fuel(fuels[-1])
+    extra_fuel.positive? ? fuels << extra_fuel : break
+  end
+  fuels.sum
+end
+
+def part_two(data)
+  (data.map { |m| calculate_total_module_fuel(m) }).sum
+end
+
+if $PROGRAM_NAME == __FILE__
+  data = read_data
+  puts part_one(data)
+  puts part_two(data)
+end
