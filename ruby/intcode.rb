e4cfd404eb70d5ea9d773208745bac2cd2dcc665
JoshKarpel <josh.karpel@gmail.com>
2019-12-02 17:15:37 -0600

    assuming all opcodes will have a fixed number of parameters (!?), can use proc arity instead of listing the number of parameters explicitly

--- ruby/intcode.rb
+++ ruby/intcode.rb
@@ -1,18 +1,14 @@
 # frozen_string_literal: true
 
 OPERATIONS = {
-  1 => [
-    3,
-    proc do |codes, parameters|
-      codes[parameters[2]] = codes[parameters[0]] + codes[parameters[1]]
+  1 =>
+    proc do |codes, a, b, target|
+      codes[target] = codes[a] + codes[b]
     end,
-  ],
-  2 => [
-    3,
-    proc do |codes, parameters|
-      codes[parameters[2]] = codes[parameters[0]] * codes[parameters[1]]
+  2 =>
+    proc do |codes, a, b, target|
+      codes[target] = codes[a] * codes[b]
     end,
-  ],
 }.freeze
 
 def execute(codes)
@@ -20,21 +16,20 @@ def execute(codes)
 
   instruction_pointer = 0
   loop do
-    instruction = codes[instruction_pointer]
+    opcode = codes[instruction_pointer]
 
     # not sure how to not special-case this...
-    return codes if instruction == 99
+    return codes if opcode == 99
 
-    unless OPERATIONS.key? instruction
-      raise ArgumentError, "Unrecognized opcode #{instruction} at address #{instruction_pointer}"
+    operation = OPERATIONS[opcode]
+    if operation.nil?
+      raise ArgumentError, "Unrecognized opcode #{opcode} at address #{instruction_pointer}"
     end
 
-    num_parameters, operation = OPERATIONS[instruction]
-
-    #puts "#{instruction} #{parameters} #{codes}"
-
+    num_parameters = operation.arity - 1
     parameters = codes[instruction_pointer + 1, num_parameters]
-    operation.call(codes, parameters)
+
+    operation.call(codes, *parameters)
 
     instruction_pointer += num_parameters + 1
   end
