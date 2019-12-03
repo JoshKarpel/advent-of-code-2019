d6ee71ace08aba2ef98850d3d532d94dd9a224a1
JoshKarpel <josh.karpel@gmail.com>
2019-12-03 00:22:29 -0600

    solve day 03 part 2; more simpler is better!

--- ruby/day_03.rb
+++ ruby/day_03.rb
@@ -15,38 +15,48 @@ def manhattan_distance(p, q)
 end
 
 DIRECTION_TO_IDX_AND_SIGN = {
-  :U => [1, 1],
-  :D => [1, -1],
-  :R => [0, 1],
-  :L => [0, -1]
+  U: [1, 1],
+  D: [1, -1],
+  R: [0, 1],
+  L: [0, -1],
 }.freeze
 
-def part_one(wire_instructions)
-  trajectories = wire_instructions.map do |instructions|
-    position = [0, 0]
-    trajectory = [position.dup]
+def calculate_trajectory_and_delays(instructions)
+  delay = 0
+  delays = {}
+  position = [0, 0]
+  trajectory = [position.dup]
 
-    instructions.each do |direction, distance|
-      idx, sign = DIRECTION_TO_IDX_AND_SIGN[direction]
-      distance.times do
-        position[idx] += sign
-        trajectory << position.dup
-      end
+  instructions.each do |direction, distance|
+    idx, sign = DIRECTION_TO_IDX_AND_SIGN[direction]
+    distance.times do
+      position[idx] += sign
+      delay += 1
+
+      p = position.dup
+      trajectory << p
+      delays[p] ||= delay
     end
-
-    trajectory
   end
 
-  intersections = (trajectories[1] & trajectories[0]) - [[0, 0]]
+  [trajectory, delays]
+end
 
+def part_one(intersections)
   (intersections.map { |intersection| manhattan_distance(intersection, [0, 0]) }).min
 end
 
-def part_two(instructions)
+def part_two(intersections, delays)
+  (intersections.map { |intersection| (delays.map { |d| d[intersection] }).sum }).min
 end
 
 if $PROGRAM_NAME == __FILE__
-  wire_instructions = read_wire_instructions
-  puts "Part One: #{part_one(wire_instructions)}"
-  puts "Part Two: #{part_two(wire_instructions)}"
+  wire_1_instructions, wire_2_instructions = read_wire_instructions
+  wire_1_trajectory, wire_1_delays = calculate_trajectory_and_delays wire_1_instructions
+  wire_2_trajectory, wire_2_delays = calculate_trajectory_and_delays wire_2_instructions
+
+  intersections = (wire_1_trajectory & wire_2_trajectory) - [[0, 0]]
+
+  puts "Part One: #{part_one(intersections)}"
+  puts "Part Two: #{part_two(intersections, [wire_1_delays, wire_2_delays])}"
 end
