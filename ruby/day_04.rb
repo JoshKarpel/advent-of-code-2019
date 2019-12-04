6757afe42eb9fbdd12f8d0b749ba7fa032eaf2bd
JoshKarpel <josh.karpel@gmail.com>
2019-12-03 23:27:38 -0600

    cleanup day 04

--- ruby/day_04.rb
+++ ruby/day_04.rb
@@ -1,37 +1,42 @@
 # frozen_string_literal: true
 
-def monotonically_increasing(x)
-  (1...x.length).each do |i|
-    return false unless x[i] >= x[i - 1]
+def monotonically_increasing(p)
+  (1...p.length).each do |i|
+    return false unless p[i] >= p[i - 1]
   end
+  true
 end
 
-def two_adjacent_same(x)
-  (0...x.length - 1).each do |i|
-    return true if x[i] == x[i + 1]
-  end
-  false
-end
-
-def part_one(range)
-  (range.select { |r| two_adjacent_same(r) && monotonically_increasing(r) }).length
-end
-
-def exactly_two_adjacent_same(x)
-  (0...x.length - 1).each do |i|
-    return true if x[i] == x[i + 1] && x[i - 1] != x[i] && x[i + 2] != x[i + 1]
+def two_adjacent_characters_match(p)
+  (0...p.length - 1).each do |i|
+    return true if p[i] == p[i + 1]
   end
   false
 end
 
-def part_two(range)
-  (range.select { |r| exactly_two_adjacent_same(r) && monotonically_increasing(r) }).length
+def part_one(passwords)
+  (passwords.select do |p|
+    two_adjacent_characters_match(p) && monotonically_increasing(p)
+  end).length
+end
+
+def exactly_two_adjacent_characters_match(p)
+  (0...p.length - 1).each do |i|
+    return true if p[i] == p[i + 1] && p[i - 1] != p[i] && p[i + 2] != p[i + 1]
+  end
+  false
+end
+
+def part_two(passwords)
+  (passwords.select do |p|
+    exactly_two_adjacent_characters_match(p) && monotonically_increasing(p)
+  end).length
 end
 
 if $PROGRAM_NAME == __FILE__
   puts 'https://adventofcode.com/2019/day/4'
 
-  range = '134792'..'675810'
-  puts "Part One: #{part_one(range)}"
-  puts "Part Two: #{part_two(range)}"
+  passwords = '134792'..'675810'
+  puts "Part One: #{part_one(passwords)}"
+  puts "Part Two: #{part_two(passwords)}"
 end
