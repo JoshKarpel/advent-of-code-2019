faf78d032b4c6b2942281c12078f63a1c349b62a
JoshKarpel <josh.karpel@gmail.com>
2019-12-04 18:33:59 -0600

    get cleverererer

--- ruby/day_04.rb
+++ ruby/day_04.rb
@@ -1,34 +1,33 @@
 # frozen_string_literal: true
 
+require 'set'
+
+require_relative 'utils'
+
 def monotonically_increasing(p)
-  p.chars.each_cons(2).all? { |a, b| b.nil? || a <= b }
+  p.chars.sort == p.chars
 end
 
-def two_adjacent_characters_match(p)
-  p.chars.each_cons(2).any? { |a, b| a == b }
+def at_least_two_characters_match(p)
+  p.chars.tally.values.any? { |v| v >= 2 }
 end
+
+def exactly_two_characters_match(p)
+  p.chars.tally.values.any? { |v| v == 2 }
+end
+
+# NB: the criteria that the matching digits are NEXT TO EACH OTHER is entirely implicit!
+# If the string is monotonic, and some digits match, those matching digits must be next to each other!
 
 def part_one(passwords)
   (passwords.select do |p|
-    two_adjacent_characters_match(p) && monotonically_increasing(p)
+    monotonically_increasing(p) && at_least_two_characters_match(p)
   end).length
-end
-
-def exactly_two_adjacent_characters_match(p)
-  # This only works in conjunction with the other condition!
-  # Otherwise, p[i - 1] != p[i] checks that the start and end of the string are the same when i = 0.
-  # But that can't be the case if you also only want two characters to match, because it would imply
-  # that the whole string is just that character.
-  # Thus, this hack!
-  (0...p.length - 1).each do |i|
-    return true if p[i] == p[i + 1] && p[i - 1] != p[i] && p[i + 2] != p[i + 1]
-  end
-  false
 end
 
 def part_two(passwords)
   (passwords.select do |p|
-    exactly_two_adjacent_characters_match(p) && monotonically_increasing(p)
+    monotonically_increasing(p) && exactly_two_characters_match(p)
   end).length
 end
 
--- ruby/utils.rb
+++ ruby/utils.rb
@@ -1,7 +1,20 @@
 # frozen_string_literal: true
-# frozen_string_literal: true
 
 def manhattan(p, q = nil)
   q = Array.new(p.length, 0) if q.nil?
   p.zip(q).reduce(0) { |acc, (r, s)| acc + (r - s).abs }
+end
+
+module Enumerable
+  def tally
+    tally = Hash.new(0)
+    each { |element| tally[element] += 1 }
+    tally
+  end
+end
+
+class Array
+  def tally
+    self.each.tally
+  end
 end
