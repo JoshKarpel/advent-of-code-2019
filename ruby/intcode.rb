fc92fb2fee8976db260fbf49ddeeb783f7ca2384
JoshKarpel <josh.karpel@gmail.com>
2019-12-08 23:34:22 -0600

    refactor intcode

--- ruby/intcode.rb
+++ ruby/intcode.rb
@@ -4,6 +4,12 @@ def read_program(path)
   path.read.split(',').map(&:to_i)
 end
 
+module ParameterMode
+  POSITION = 0
+  IMMEDIATE = 1
+  RELATIVE = 2
+end
+
 class Intcode
   attr_reader :program, :inputs, :outputs, :halted
 
@@ -16,9 +22,7 @@ class Intcode
 
     @outputs = []
     @instruction_pointer = 0
-
     @relative_base = 0
-
     @halted = false
   end
 
@@ -41,15 +45,6 @@ class Intcode
     end
   end
 
-  def operation(opcode)
-    op = method("do_#{opcode}".to_sym)
-    if op.nil?
-      raise ArgumentError, "Unrecognized opcode #{opcode} at address #{@instruction_pointer}"
-    end
-
-    op
-  end
-
   def state
     opcode, modes = opcode_and_modes
     op = operation opcode
@@ -66,98 +61,112 @@ class Intcode
     ].join(' ')
   end
 
-  def opcode_and_modes
-    instruction = @program[@instruction_pointer].to_s
-    if instruction.length <= 2
-      opcode = instruction.to_i
-      modes = []
-    else
-      opcode = instruction[-2, 2].to_i
-      modes = instruction[0...-2].reverse.chars.map(&:to_i)
-    end
-    [opcode, modes]
-  end
+  private
 
-  def raw_parameters(op)
-    params = []
-    op.arity.times do |idx|
-      params << @program[@instruction_pointer + 1 + idx] || 0
-    end
-    params
-  end
+    def operation(opcode)
+      op = method("do_#{opcode}".to_sym)
+      if op.nil?
+        raise ArgumentError, "Unrecognized opcode #{opcode} at address #{@instruction_pointer}"
+      end
 
-  def parameters(operation, modes)
-    raw_parameters(operation)
-      .zip(modes, operation.parameters.map(&:last).map(&:to_s))
-      .map do |raw_param, mode, param_name|
-      case mode
-        when 0, nil
-          param_name.start_with?('w_') ? raw_param : @program[raw_param]
-        when 1
-          raw_param
-        when 2
-          param_name.start_with?('w_') ? raw_param + @relative_base : @program[raw_param + @relative_base]
-        else
-          raise ArgumentError, "Unrecognized mode #{mode} for instruction #{@instruction_pointer}"
+      op
+    end
+
+    def opcode_and_modes
+      instruction = @program[@instruction_pointer].to_s
+      if instruction.length <= 2
+        opcode = instruction.to_i
+        modes = []
+      else
+        opcode = instruction[-2, 2].to_i
+        modes = instruction[0...-2].reverse.chars.map(&:to_i)
+      end
+      [opcode, modes]
+    end
+
+    def parameters(operation, modes)
+      raw_parameters(operation)
+        .zip(modes, operation.parameters.map(&:last).map(&:to_s))
+        .map do |raw_param, mode, param_name|
+        parameter_value(mode, param_name, raw_param)
       end
     end
-  end
 
-  # OPERATIONS
+    def raw_parameters(op)
+      params = []
+      op.arity.times do |idx|
+        params << @program[@instruction_pointer + 1 + idx] || 0
+      end
+      params
+    end
 
-  # add
-  def do_1(a, b, w_target)
-    @program[w_target] = a + b
-    nil
-  end
+    def parameter_value(mode, param_name, raw_param)
+      return raw_param if mode == ParameterMode::IMMEDIATE
 
-  # multiply
-  def do_2(a, b, w_target)
-    @program[w_target] = a * b
-    nil
-  end
+      raw_param += @relative_base if mode == ParameterMode::RELATIVE
 
-  # input
-  def do_3(w_target)
-    @program[w_target] = @inputs.shift
-    nil
-  end
+      # semantics of POSITION and IMMEDIATE are the same at this point
 
-  # output
-  def do_4(output)
-    @outputs << output
-    nil
-  end
+      return raw_param if param_name.start_with?('w_')
 
-  # jump-if-true
-  def do_5(test, pointer)
-    test.nonzero? ? pointer : nil
-  end
+      @program[raw_param]
+    end
 
-  # jump-if-false
-  def do_6(test, pointer)
-    test.zero? ? pointer : nil
-  end
+    # OPERATIONS
 
-  # less-than
-  def do_7(left, right, w_target)
-    @program[w_target] = left < right ? 1 : 0
-    nil
-  end
+    # add
+    def do_1(a, b, w_target)
+      @program[w_target] = a + b
+      nil
+    end
 
-  # equals
-  def do_8(left, right, w_target)
-    @program[w_target] = left == right ? 1 : 0
-    nil
-  end
+    # multiply
+    def do_2(a, b, w_target)
+      @program[w_target] = a * b
+      nil
+    end
 
-  # adjust relative base
-  def do_9(adjustment)
-    @relative_base += adjustment
-    nil
-  end
+    # input
+    def do_3(w_target)
+      @program[w_target] = @inputs.shift
+      nil
+    end
 
-  # break (dummy method, actually implemented in #execute, just here for #state)
-  def do_99
-  end
+    # output
+    def do_4(output)
+      @outputs << output
+      nil
+    end
+
+    # jump-if-true
+    def do_5(test, pointer)
+      test.nonzero? ? pointer : nil
+    end
+
+    # jump-if-false
+    def do_6(test, pointer)
+      test.zero? ? pointer : nil
+    end
+
+    # less-than
+    def do_7(left, right, w_target)
+      @program[w_target] = left < right ? 1 : 0
+      nil
+    end
+
+    # equals
+    def do_8(left, right, w_target)
+      @program[w_target] = left == right ? 1 : 0
+      nil
+    end
+
+    # adjust relative base
+    def do_9(adjustment)
+      @relative_base += adjustment
+      nil
+    end
+
+    # break (dummy method, actually implemented in #execute, just here for #state)
+    def do_99
+    end
 end
