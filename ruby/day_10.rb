489e3b27071f988866e4d88bf1454e714f4e52c2
JoshKarpel <josh.karpel@gmail.com>
2019-12-10 23:15:27 -0600

    cleaner solution to day 10

--- ruby/day_10.rb
+++ ruby/day_10.rb
@@ -14,67 +14,76 @@ end
 def angle(p, q)
   dx = p[0] - q[0]
   dy = p[1] - q[1]
-  #if dx.zero?
-  #  return [1, 0] if dy.positive?
-  #  return [-1, 0] if dy.negative?
-  #end
-  lcm = dy.gcd dx
-  return [dy, dx] if lcm.zero?
+  gcd = dy.gcd dx
+  return [dy, dx] if gcd.zero?
 
-  [dy / lcm, dx / lcm]
+  [dy / gcd, dx / gcd]
 end
 
-def part_one(asteroids)
+def asteroid_positions(asteroids)
   cols = asteroids.length
   rows = asteroids[0].length
 
-  angles = Hash.new { |h, k| h[k] = Set.new }
+  positions = block_given? ? nil : []
   rows.times do |x|
     cols.times do |y|
       next if asteroids[y][x] == '.'
 
-      rows.times do |x2|
-        cols.times do |y2|
-          next if asteroids[y2][x2] == '.' || (x == x2 && y == y2)
-
-          angles[[x, y]] << angle([x, y], [x2, y2])
-        end
+      if block_given?
+        yield [x, y]
+      else
+        positions << [x, y]
       end
     end
   end
-  _, most_angles = angles.max_by { |_, a| a.length }
-  puts _.to_s
-  most_angles.length
+  positions
 end
 
-def part_two(asteroids, laser)
-  cols = asteroids.length
-  rows = asteroids[0].length
+def visible_asteroids(asteroids, position)
+  asteroids_by_angle = Hash.new { |h, k| h[k] = [] }
 
-  angles = Hash.new { |h, k| h[k] = [] }
-  rows.times do |x2|
-    cols.times do |y2|
-      next if asteroids[y2][x2] == '.' || (laser[0] == x2 && laser[1] == y2)
+  asteroid_positions(asteroids) do |target|
+    next if position == target
 
-      angles[angle(laser, [x2, y2])] << [x2, y2]
+    asteroids_by_angle[angle(position, target)] << target
+  end
+
+  asteroids_by_angle.each do |_, v|
+    v.sort_by! { |e| manhattan(e, position) }
+  end
+
+  asteroids_by_angle
+end
+
+def find_best_location(asteroids)
+  asteroid_positions(asteroids).max_by do |monitor|
+    visible_asteroids(asteroids, monitor).keys.length
+  end
+end
+
+def part_one(asteroids)
+  best_location = find_best_location(asteroids)
+  visible_asteroids(asteroids, best_location).keys.length
+end
+
+def part_two(asteroids)
+  laser = find_best_location(asteroids)
+
+  asteroids_by_angle = visible_asteroids(asteroids, laser).map do |a, k|
+    [Math.atan2(*a), k]
+  end
+  asteroids_by_angle.sort_by!(&:first)
+  new_start = asteroids_by_angle.map(&:first).find_index { |angle| angle >= Math::PI / 2 }
+  roids = asteroids_by_angle.map(&:last).rotate(new_start)
+
+  destruction_order = []
+  while roids.map(&:length).any?(&:positive?)
+    roids.filter { |r| !r.length.zero? }.each do |r|
+      destruction_order << r.shift
     end
   end
 
-  ordered_angles = angles.map { |a, k| [(Math.atan2(*a) - (Math::PI / 2)), k.sort_by { |z| manhattan(z, laser) }] }.sort_by! { |a, _| a }
-
-  first_angle_idx = ordered_angles.find_index { |a, _| a.positive? }
-  ordered_angles = ordered_angles[(first_angle_idx - 1)..-1] + ordered_angles[0...first_angle_idx - 1]
-
-  ordered_asteroids = []
-  while ordered_angles.map(&:last).map(&:length).any?(&:positive?)
-    ordered_angles.each do |_k, v|
-      next if v.length.zero?
-
-      ordered_asteroids << v.shift
-    end
-  end
-
-  x, y = ordered_asteroids[199]
+  x, y = destruction_order[199]
   (x * 100) + y
 end
 
@@ -84,5 +93,5 @@ if $PROGRAM_NAME == __FILE__
   asteroids = read_asteroids
 
   puts "Part One: #{part_one(asteroids)}"
-  puts "Part Two: #{part_two(asteroids, [11, 11])}"
+  puts "Part Two: #{part_two(asteroids)}"
 end
