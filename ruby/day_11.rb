d7bebd8d71129a7b6aff3f09895ad8bcce344821
JoshKarpel <josh.karpel@gmail.com>
2019-12-11 00:01:17 -0600

    solve day 11 part 2

--- ruby/day_11.rb
+++ ruby/day_11.rb
@@ -11,17 +11,19 @@ FACING = {
   0 => -1i,
   1 => +1i,
 }.freeze
+
 MOVE = {
   1 + 0i => Vector[1, 0],
-  1i => Vector[0, 1],
+  0 + 1i => Vector[0, 1],
   -1 + 0i => Vector[-1, 0],
-  -1i => Vector[0, -1],
+  0 - 1i => Vector[0, -1],
 }.freeze
 
-def part_one(program)
+def paint_hull(program, starting_color)
   brain = Intcode.new(program)
   hull = Hash.new(0)
   position = Vector[0, 0]
+  hull[position] = starting_color
   facing = 1i
 
   loop do
@@ -35,13 +37,36 @@ def part_one(program)
 
     hull[position] = color
     facing *= FACING[turn]
-    position += MOVE[facing]
+    position -= MOVE[facing] # TODO: inverted moves for some reason?
   end
 
-  hull.keys.length
+  hull.map { |k, v| [k.to_a, v] }.to_h
 end
 
+def part_one(program)
+  paint_hull(program, 0).keys.length
+end
+
+CHAR = {
+  0 => '░',
+  1 => '█',
+}.freeze
+
 def part_two(program)
+  hull = paint_hull(program, 1)
+  min_x = hull.keys.map(&:first).min
+  max_x = hull.keys.map(&:first).max
+  min_y = hull.keys.map(&:last).min
+  max_y = hull.keys.map(&:last).max
+
+  msg = []
+  (min_y..max_y).each do |y|
+    (min_x..max_x).each do |x|
+      msg << CHAR[hull[[x, y]] || 0]
+    end
+    msg << "\n"
+  end
+  "\n" + msg.join
 end
 
 if $PROGRAM_NAME == __FILE__
