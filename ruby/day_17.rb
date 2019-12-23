f7572b07806f0857e8784e5c3efd480d5d14efe8
Josh Karpel <josh.karpel@gmail.com>
2019-12-23 16:04:01 -0600

    started day 17 part 2, not done yet

--- ruby/day_17.rb
+++ ruby/day_17.rb
@@ -8,7 +8,7 @@ require_relative 'intcode'
 require_relative 'utils'
 
 def part_one(program)
-  camera = Intcode.new(program).execute
+  camera = Intcode.new(program).execute!
 
   view = camera.outputs.map(&:chr)
   puts view.join
@@ -29,36 +29,42 @@ def part_one(program)
   scores.sum
 end
 
+def find_path(program)
+  camera = Intcode.new(program).execute!
+
+  view = camera.outputs.map(&:chr)
+  puts view.join
+
+  screen_by_row = view.join.split("\n")
+
+  start_r, start_c = find_start(screen_by_row)
+
+  cmds = [0]
+  total = view.tally['#']
+  puts total
+  visited = 1
+  while visited < total
+
+  end
+
+  puts [start_r, start_c].to_s
+end
+
+def find_start(screen_by_row)
+  screen_by_row.length.times do |r|
+    screen_by_row[0].length.times do |c|
+      return [r, c] if screen_by_row[r][c] == '^'
+    end
+  end
+end
+
 def as_ascii(commands)
   commands.join(',').each_char.map { |c| c.is_a?(Integer) ? c : c.ord } + ["\n".ord]
 end
 
 def part_two(program)
-  # MAIN
-  main = %w[A B C B]
-  # A
-  #a = [5]
-  a = ['L', 8, 'R', 10, 'L', 'L']
-  # B
-  b = ['R', 8, 'R', 8]
-  # C
-  c = ['L', 12, 'R', 8]
-  # VIDEO
-  video = ['n']
+  find_path(program)
 
-  program[0] = 2
-  inputs = [main, a, b, c, video].map { |c| as_ascii(c) }.flatten
-  puts inputs.to_s
-  camera = Intcode.new(program, inputs)
-
-  loop do
-    camera.execute(true)
-    break if camera.outputs[-1] > 256 || camera.halted
-
-    print camera.outputs[-1].chr
-  end
-
-  camera.outputs[-1]
 end
 
 if $PROGRAM_NAME == __FILE__
