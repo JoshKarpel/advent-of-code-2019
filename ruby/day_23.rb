b0c0689ed9aebc19efa0b41d95feb6ce98d55f6f
Josh Karpel <josh.karpel@gmail.com>
2019-12-23 16:53:41 -0600

    clean up day 23

--- ruby/day_23.rb
+++ ruby/day_23.rb
@@ -8,42 +8,50 @@ require 'set'
 require_relative 'intcode'
 require_relative 'utils'
 
+SLEEP_DURATION = 0.001
+
+def spawn_computer(program, addr, input_queue, router)
+  Thread.new do
+    computer = Intcode.new(program, [addr])
+    loop do
+      sleep SLEEP_DURATION
+
+      computer.execute!(true, true)
+
+      if computer.outputs.size == 1 # send
+        computer.execute!(true).execute!(true)
+        router << computer.outputs.shift(3)
+      elsif input_queue.size.positive?
+        computer.inputs << input_queue.pop
+        computer.inputs << input_queue.pop
+      else
+        computer.inputs << -1
+      end
+    end
+  end
+end
+
 def part_one(program)
   router = Queue.new
 
   inputs = 50.times.map { |_| Queue.new }
 
   nics = inputs.each_with_index.map do |input_queue, addr|
-    Thread.new do
-      computer = Intcode.new(program, [addr])
-      loop do
-        sleep 0.01
-
-        computer.execute!(true, true)
-
-        if computer.outputs.size == 1 # send
-          computer.execute!(true).execute!(true)
-          router << computer.outputs.shift(3)
-        else # empty receive
-          if input_queue.size.positive?
-            computer.inputs << input_queue.pop
-            computer.inputs << input_queue.pop
-          else
-            computer.inputs << -1
-          end
-        end
-      end
-    end
+    spawn_computer(program, addr, input_queue, router)
   end
 
   loop do
-    addr, x, y = router.shift
+    sleep SLEEP_DURATION
+
+    addr, x, y = router.pop
+
     if addr == 255
       nics.map(&:kill)
       return y
+    else
+      inputs[addr] << x
+      inputs[addr] << y
     end
-    inputs[addr] << x
-    inputs[addr] << y
   end
 end
 
@@ -53,39 +61,22 @@ def part_two(program)
   inputs = 50.times.map { |_| Queue.new }
 
   nics = inputs.each_with_index.map do |input_queue, addr|
-    Thread.new do
-      computer = Intcode.new(program, [addr])
-      loop do
-        sleep 0.001
-
-        computer.execute!(true, true)
-
-        if computer.outputs.size == 1 # send
-          computer.execute!(true).execute!(true)
-          router << computer.outputs.shift(3)
-        else # empty receive
-          if input_queue.size.positive?
-            computer.inputs << input_queue.pop
-            computer.inputs << input_queue.pop
-          else
-            computer.inputs << -1
-          end
-        end
-      end
-    end
+    spawn_computer(program, addr, input_queue, router)
   end
 
   nat = [nil, nil]
   idle_count = 0
   last_y_sent = nil
   loop do
-    sleep 0.001
+    sleep SLEEP_DURATION
 
     begin
       packet = router.pop(true)
     rescue ThreadError
       idle_count += 1
-      if inputs.all?(&:empty?) && idle_count > 100
+
+      # if no inputs to be processed and the router has been idle for 10 cycles, fire the NAT
+      if inputs.all?(&:empty?) && idle_count > 10
         inputs[0] << nat[0]
         inputs[0] << nat[1]
 
